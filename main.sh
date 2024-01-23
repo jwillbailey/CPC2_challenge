55a2696db1a2b00a9d22f1f337d2b103f33de0a1
RhiM1 <72741511+RhiM1@users.noreply.github.com>
2024-01-23 11:34:35 +0000

    Update script

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 1 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 2 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 3 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
+# Base models
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 1 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 2 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM_layers --layer -1 --N 3 --exp_id base --lr 0.00001 --weight_decay 0.0001 --train_disjoint --n_epochs 25
 
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 1 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 2 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
-CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 3 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
+# Exemplar models
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 1 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 2 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
+python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model ExLSTM_layers --layer -1 --N 3 --exp_id ex --ex_size 8 --lr 0.000002  --weight_decay 0.0001 --train_disjoint --n_epochs 50
